d66f8ffb19fe913fd4571dbea4314a3bab98936f
YuvrajBhardwaj <yuvrajbhardwaj245@gmail.com>
2024-07-01 01:48:43 +0530

    Add project files.

---
+++ Index.aspx
@@ -0,0 +1,146 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="AddUpdateEmployee.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 450px;
+            border: 5px black ridge;
+        }
+
+        .auto-style2 {
+            width: 200px;
+        }
+
+        .auto-style3 {
+            width: 200px;
+            height: 37px;
+        }
+
+        .auto-style4 {
+            height: 40px;
+            text-align: center;
+        }
+
+        #GridView1 {
+            width: 550px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Status" style="color: crimson; font-size:x-large;" runat="server" Text="Index"></asp:Label>
+            <table cellpadding="4" cellspacing="4" class="auto-style1" align="center">
+                <tr>
+                    <td colspan="2">
+                        <h1 style="text-align:center">EMPLOYEE TABLE &nbsp;</h1>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">ID:</td>
+                    <td>
+                        <asp:TextBox ID="idtxt" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator5" ValidationGroup="Validate" runat="server" ControlToValidate="idtxt" ErrorMessage="ID is Required" ForeColor="Red" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">FIRST NAME:</td>
+                    <td>
+                        <asp:TextBox ID="firstnametxt" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ValidationGroup="Validate" ControlToValidate="firstnametxt" ErrorMessage="Firstname is Required" ForeColor="Red" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">LAST NAME:</td>
+                    <td>
+                        <asp:TextBox ID="lastnametxt" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ValidationGroup="Validate" ControlToValidate="lastnametxt" ErrorMessage="Lastname is Required" ForeColor="Red" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">AGE:</td>
+                    <td>
+                        <asp:TextBox ID="agetxt" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ValidationGroup="Validate" ControlToValidate="agetxt" ErrorMessage="Age is Required" ForeColor="Red" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">MOBILE NO:</td>
+                    <td>
+                        <asp:TextBox ID="mobilenotxt" runat="server"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator4" runat="server" ControlToValidate="mobilenotxt" ValidationGroup="Validate" ErrorMessage="Mobile No is Required" ForeColor="Red" SetFocusOnError="True">*</asp:RequiredFieldValidator>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style3">PROFILE PIC:</td>
+                    <td class="auto-style4">
+                        <asp:Image ID="GetImage" Height="70" Width="70" Visible="false" runat="server" />
+                        <br />
+                        <asp:FileUpload ID="FileUpload1" runat="server" />
+                        <asp:Label ID="Label1" runat="server" ForeColor="Red" Text="Label" Visible="False"></asp:Label>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4" colspan="2">
+                        <asp:Button ID="insertbtn" runat="server" Text="INSERT" BorderColor="Green" ValidationGroup="Validate" BackColor="Green" ForeColor="White" OnClick="insertbtn_Click" />
+                        <asp:Button ID="updatebtn" runat="server" Text="UPDATE" BorderColor="Blue" ValidationGroup="Validate" BackColor="Blue" ForeColor="White" OnClick="updatebtn_Click" />
+                        <asp:Button ID="deletebtn" runat="server" Text="DELETE" BorderColor="Red" BackColor="Red" ForeColor="White" OnClick="deletebtn_Click" />
+                        <asp:Button ID="resetbtn" runat="server" Text="RESET" BorderColor="Orange" BackColor="Orange" ForeColor="White" OnClick="resetbtn_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4" colspan="2">
+                        <asp:ValidationSummary ID="ValidationSummary1" runat="server" ValidationGroup="Validate" BackColor="Silver" ForeColor="Red" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" HorizontalAlign="Center" AutoGenerateColumns="False" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+                <AlternatingRowStyle BackColor="White" />
+                <Columns>
+                    <asp:CommandField ShowSelectButton="True" />
+                    <asp:TemplateField HeaderText="ID">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="labelID" Text='<%# Eval("id") %>'></asp:Label></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="First Name">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="labelFirstname" Text='<%# Eval("firstname") %>'></asp:Label></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Last Name">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="labelLastname" Text='<%# Eval("lastname") %>'></asp:Label></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Age">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="labelAge" Text='<%# Eval("age") %>'></asp:Label></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Mobile No">
+                        <ItemTemplate>
+                            <asp:Label runat="server" ID="labelMobile" Text='<%# Eval("mobileno") %>'></asp:Label></ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Profile Pic">
+                        <ItemTemplate>
+                            <asp:Image ID="Image1" Height="100" Width="100" ImageUrl='<%# Eval("profilepic") %>' runat="server" />
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                <EditRowStyle BackColor="#2461BF" />
+                <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#EFF3FB" />
+                <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                <SortedDescendingHeaderStyle BackColor="#4870BE" />
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace AddUpdateEmployee
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["dbcs"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        protected void insertbtn_Click(object sender, EventArgs e)
+        {
+            //Response.Redirect("Insert.aspx");
+            SqlConnection con = new SqlConnection(cs);
+
+            string folderPath = Server.MapPath("ProfilePic/");
+
+            string filename = Path.GetFileName(FileUpload1.FileName);
+            string extension = Path.GetExtension(filename);
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            int size = postedFile.ContentLength;
+
+            if (FileUpload1.HasFile == true || GetImage.ImageUrl != null)
+            {
+                if (extension.ToLower() == ".jpg" || extension.ToLower() == ".png" || extension.ToLower() == ".jpeg" || GetImage.ImageUrl != null)
+                {
+                    if (size < 10000000 || GetImage.ImageUrl != null) 
+                    {
+                        string query2 = "select * from Employee_Master where id = @id";
+                        SqlCommand cmd2 = new SqlCommand(query2, con);
+                        cmd2.Parameters.AddWithValue("@id", idtxt.Text);
+                        con.Open();
+                        SqlDataReader dr = cmd2.ExecuteReader();
+                        if (dr.HasRows == true)
+                        {
+                            Response.Write("<script>alert('ID Already Inserted!')</script>");
+                            con.Close();
+                        }
+                        else
+                        {
+
+                            con.Close();
+                            string path2 = string.Empty;
+                            if (GetImage.ImageUrl == null || GetImage.ImageUrl == "")
+                            {
+                                FileUpload1.SaveAs(folderPath + filename);
+                                path2 = "ProfilePic/" + filename;
+                            }
+                            else
+                            {
+                                path2 = GetImage.ImageUrl.ToString(); 
+                            }
+                            
+                            
+                            string query = "insert into Employee_Master values(@id,@firstname, @lastname, @age, @mobileno, @profilepic)";
+                            SqlCommand cmd = new SqlCommand(query, con);
+                            cmd.Parameters.AddWithValue("@id", idtxt.Text);
+                            cmd.Parameters.AddWithValue("@firstname", firstnametxt.Text);
+                            cmd.Parameters.AddWithValue("@lastname", lastnametxt.Text);
+                            cmd.Parameters.AddWithValue("@age", agetxt.Text);
+                            cmd.Parameters.AddWithValue("@mobileno", mobilenotxt.Text);
+                            cmd.Parameters.AddWithValue("@profilepic", path2);
+
+                            con.Open();
+                            int a = cmd.ExecuteNonQuery();
+                            if (a > 0)
+                            {
+                                //Response.Write("<script>alert('Inserted Successfully')</script>");
+                                Status.Text = "Inserted Successfully";
+                                Label1.Visible = false;
+                                ResetControls();
+                                BindGridView();
+                            }
+                            else
+                            {
+                                //Response.Write("<script>alert('Insert Failed!!! ')</script>");
+                                Status.Text = "Insert Failed!!!";
+                            }
+                            con.Close();
+                        }
+
+                    }
+                    else
+                    {
+                        Label1.Text = "File Size should be less than 10 MB";
+                        Label1.Visible = true;
+                    }
+                }
+                else
+                {
+                    Label1.Text = "Please Upload Profile Picture in a valid format.";
+                    Label1.Visible = true;
+                }
+            }
+            else
+            {
+                Label1.Text = "Please Upload Profile Picture";
+                Label1.Visible = true;
+                ValidationSummary1.Visible = true;
+                RequiredFieldValidator1.Visible = true;
+                RequiredFieldValidator2.Visible = true;
+                RequiredFieldValidator3.Visible = true;
+                RequiredFieldValidator4.Visible = true;
+                RequiredFieldValidator5.Visible = true;
+
+            }
+        }
+
+        void ResetControls()
+        {
+            idtxt.Text = firstnametxt.Text = lastnametxt.Text = agetxt.Text = mobilenotxt.Text = "";
+            Label1.Visible = false;
+            GetImage.Visible = false;
+            GridView1.SelectedIndex = -1;
+
+
+        }
+        protected void updatebtn_Click(object sender, EventArgs e)
+        {
+            //Response.Redirect("Update.aspx");
+            SqlConnection con = new SqlConnection(cs);
+
+            string folderPath = Server.MapPath("ProfilePic/");
+
+            string filename = Path.GetFileName(FileUpload1.FileName);
+            string extension = Path.GetExtension(filename);
+            HttpPostedFile postedFile = FileUpload1.PostedFile;
+            int size = postedFile.ContentLength;
+
+            string UpdatePath = "ProfilePic/";
+            if (FileUpload1.HasFile == true)
+            {
+                if (extension.ToLower() == ".jpg" || extension.ToLower() == ".png" || extension.ToLower() == ".jpeg")
+                {
+                    if (size < 10000000)
+                    {
+                        UpdatePath = UpdatePath + filename;
+                        FileUpload1.SaveAs(Server.MapPath(UpdatePath));
+
+                        string query = "update Employee_Master set firstname= @firstname, lastname= @lastname, age=@age, mobileno= @mobileno, profilepic=@profilepic where id=@id";
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@id", idtxt.Text);
+                        cmd.Parameters.AddWithValue("@firstname", firstnametxt.Text);
+                        cmd.Parameters.AddWithValue("@lastname", lastnametxt.Text);
+                        cmd.Parameters.AddWithValue("@age", agetxt.Text);
+                        cmd.Parameters.AddWithValue("@mobileno", mobilenotxt.Text);
+                        cmd.Parameters.AddWithValue("@profilepic", UpdatePath);
+
+
+                        con.Open();
+                        int a = cmd.ExecuteNonQuery();
+                        if (a > 0)
+                        {
+                            //Response.Write("<script>alert('Updated Successfully')</script>");
+                            Status.Text = "Updated Successfully";
+                            Label1.Visible = false;
+                            ResetControls();
+                            BindGridView();
+                        }
+                        else
+                        {
+                            //Response.Write("<script>alert('Update Failed!!! ')</script>");
+                            Status.Text = "Update Failed!!!";
+                        }
+
+                        con.Close();
+
+
+                    }
+                    else
+                    {
+                        Label1.Text = "File Size should be less than 10 MB";
+                        Label1.Visible = true;
+                    }
+                }
+                else
+                {
+                    Label1.Text = "Please Upload Profile Picture in a valid format.";
+                    Label1.Visible = true;
+                }
+            }
+            else
+            {
+                UpdatePath = GetImage.ImageUrl.ToString();
+                string query = "update Employee_Master set firstname= @firstname, lastname= @lastname, age=@age, mobileno= @mobileno, profilepic=@profilepic where id=@id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", idtxt.Text);
+                cmd.Parameters.AddWithValue("@firstname", firstnametxt.Text);
+                cmd.Parameters.AddWithValue("@lastname", lastnametxt.Text);
+                cmd.Parameters.AddWithValue("@age", agetxt.Text);
+                cmd.Parameters.AddWithValue("@mobileno", mobilenotxt.Text);
+                cmd.Parameters.AddWithValue("@profilepic", UpdatePath);
+
+
+                con.Open();
+                int a = cmd.ExecuteNonQuery();
+                if (a > 0)
+                {
+                    //Response.Write("<script>alert('Updated Successfully')</script>");
+                    Status.Text = "Updated Successfully";
+                    Label1.Visible = false;
+                    ResetControls();
+                    BindGridView();
+                }
+                else
+                {
+                    //Response.Write("<script>alert('Update Failed!!! ')</script>");
+                    Status.Text = "Update Failed!!!";
+                }
+
+                con.Close();
+            }
+
+        }
+
+        void BindGridView()
+        {
+            SqlConnection con = new SqlConnection(cs);
+            string query = "select * from Employee_Master";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            GridView1.DataSource = data;
+            GridView1.DataBind();
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = GridView1.SelectedRow;
+
+            Label lblId = (Label)row.FindControl("labelID");
+            Label lblFirstname = (Label)row.FindControl("labelFirstname");
+            Label lblLastname = (Label)row.FindControl("labelLastname");
+            Label lblAge = (Label)row.FindControl("labelAge");
+            Label lblMobile = (Label)row.FindControl("labelMobile");
+            Image img = (Image)row.FindControl("Image1");
+
+            idtxt.Text = lblId.Text;
+            firstnametxt.Text = lblFirstname.Text;
+            lastnametxt.Text = lblLastname.Text;
+            agetxt.Text = lblAge.Text;
+            mobilenotxt.Text = lblMobile.Text;
+            GetImage.ImageUrl = img.ImageUrl;
+            GetImage.Visible = true;
+        }
+
+        protected void deletebtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "delete from Employee_Master where id=@id";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@id", idtxt.Text);
+
+
+
+            con.Open();
+            int a = cmd.ExecuteNonQuery();
+            if (a > 0)
+            {
+                //Response.Write("<script>alert('Deleted Successfully')</script>");
+                Status.Text = "Deleted Successfully";
+                Label1.Visible = false;
+                GetImage.Visible = false;
+                ResetControls();
+                BindGridView();
+
+                string DeletePath = Server.MapPath(GetImage.ImageUrl.ToString());
+                if (File.Exists(DeletePath) == true)
+                {
+                    File.Delete(DeletePath);
+                }
+            }
+            else
+            {
+                //Response.Write("<script>alert('Delete Failed!!! ')</script>");
+                Status.Text = "Delete Failed!!!";
+            }
+            con.Close();
+        }
+
+        protected void resetbtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Index.aspx", true);
+        }
+    }
+}
